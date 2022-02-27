0357504568cd9339bc7dc31d5e6c86196dc145a3
Yaroslav.S <sysoev.yar@yandex.ru>
2022-02-27 23:07:09 +0500

    Сделал домашнее задание № 4

---
+++ 3.HomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N (например, 8 -> 2, 4, 6, 8).
+// Решение домашнего задания № 4.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string strValue = Console.ReadLine();
+int Value = int.Parse(strValue);
+int index = 1;
+
+Console.WriteLine("------------------");
+while (index <= Value)
+{
+    if (index%2 == 0)
+    {
+        Console.Write($"{index}, ");
+    }
+    index++;
+}
