b5e671992f763e95216308b4d63b33e993668581
Yaroslav.S <sysoev.yar@yandex.ru>
2022-02-27 19:16:26 +0500

    Пример 2.1

--- 1.example/Program.cs
+++ 1.example/Program.cs
@@ -1,4 +1,5 @@
 ﻿// пример из семинара 1. Напишите программу, которая на вход принимает 2 числа и проверяет, является ли 1 число квадратом 2 числа.
+
 Console.Write("Введите число 1: ");
 string strValue1 = Console.ReadLine();
 int Value1 = int.Parse(strValue1);
@@ -13,5 +14,7 @@ if (rezult == Value2)
 {
     Console.WriteLine($"Число {Value1} является квадратом числа {Value2}");
 }
-
-Console.WriteLine($"Число {Value1} не является квадратом числа {Value2}");
+else
+{
+    Console.WriteLine($"Число {Value1} не является квадратом числа {Value2}");
+}
