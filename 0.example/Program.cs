11d7dfec002899cdbf699c59fc2a0bf83ecee926
Yaroslav.S <sysoev.yar@yandex.ru>
2022-02-27 17:14:21 +0500

    Пример 1

---
+++ 0.example/Program.cs
@@ -0,0 +1,8 @@
+﻿// пример из семинара 0. Напишите программу, которая на вход принимает число и выдет квадрат этого числа. 4-16, -9-81 и т.д.
+
+Console.Write("Введите число: ");
+string strValue = Console.ReadLine(); //ввод строки
+int value = int.Parse(strValue);    //преобразуем строку в целое число
+
+int rezult = value * value;         //вычисление квадрата
+Console.Write($"Квадрат числа {value} равен {rezult}");
